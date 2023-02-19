a80aef652a440624e0582f3891b69eeb346bb945
kedziorno <kjoblak85@gmail.com>
2023-02-19 17:33:52 +0100

    ExtractKsToScaleParameter

--- ExtractKsToScaleParameter.vhd
+++ ExtractKsToScaleParameter.vhd
@@ -33,8 +33,14 @@ entity ExtractKsToScaleParameter is
 port (
 i_clock : IN  std_logic;
 i_reset : IN  std_logic;
-i_ee0x243f : IN  std_logic_vector (15 downto 0);
-o_kstoscale : OUT  std_logic_vector (31 downto 0)
+i_run : in std_logic;
+
+i2c_mem_ena : out STD_LOGIC;
+i2c_mem_addra : out STD_LOGIC_VECTOR(11 DOWNTO 0);
+i2c_mem_douta : in STD_LOGIC_VECTOR(7 DOWNTO 0);
+
+o_kstoscale : OUT  std_logic_vector (31 downto 0);
+o_rdy : out std_logic
 );
 end ExtractKsToScaleParameter;
 
@@ -45,12 +51,41 @@ signal address_kstoscale : std_logic_vector (8 downto 0);
 
 begin
 
-p0 : process (i_ee0x243f) is
+p0 : process (i_clock) is
+	type states is (idle,
+	s1,s2,s3,s4,s5,
+	ending);
+	variable state : states;
 begin
-	address_kstoscale <= std_logic_vector (to_unsigned (to_integer (unsigned (i_ee0x243f (3 downto 0))),9));
+	if (rising_edge (i_clock)) then
+		if (i_reset = '1') then
+			state := idle;
+			i2c_mem_ena <= '0';
+			o_rdy <= '0';
+		else
+			case (state) is
+				when idle =>
+					if (i_run = '1') then
+						state := s1;
+						i2c_mem_ena <= '1';
+					else
+						state := idle;
+						i2c_mem_ena <= '0';
+					end if;
+				when s1 => state := s2;
+					i2c_mem_addra <= std_logic_vector (to_unsigned (63*2+1, 12)); -- ee243f LSB kstoscale 0x000f
+				when s2 => state := s3;
+				when s3 => state := s4;
+					address_kstoscale <= "00000"&i2c_mem_douta (3 downto 0);
+				when s4 => state := s5;
+				when s5 => state := ending;
+					o_kstoscale <= odata_kstoscale;
+				when ending => state := idle;
+					o_rdy <= '1';
+			end case;
+		end if;
+	end if;
 end process p0;
-
-o_kstoscale <= odata_kstoscale;
 
 inst_mem_kstoscale : RAMB16_S36
 generic map (
--- tb_ExtractKsToScaleParameter.vhd
+++ tb_ExtractKsToScaleParameter.vhd
@@ -35,62 +35,90 @@ USE ieee.std_logic_1164.ALL;
 ENTITY tb_ExtractKsToScaleParameter IS
 END tb_ExtractKsToScaleParameter;
 
-ARCHITECTURE behavior OF tb_ExtractKsToScaleParameter IS 
+ARCHITECTURE behavior OF tb_ExtractKsToScaleParameter IS
+
+COMPONENT tb_i2c_mem
+PORT (
+clka : IN STD_LOGIC;
+ena : IN STD_LOGIC;
+wea : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+addra : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
+dina : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+douta : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+);
+END COMPONENT;
 
 -- Component Declaration for the Unit Under Test (UUT)
-
 COMPONENT ExtractKsToScaleParameter
 PORT(
 i_clock : IN  std_logic;
 i_reset : IN  std_logic;
-i_ee0x243f : IN  std_logic_vector(15 downto 0);
-o_kstoscale : OUT  std_logic_vector(31 downto 0)
+i_run : in std_logic;
+i2c_mem_ena : out STD_LOGIC;
+i2c_mem_addra : out STD_LOGIC_VECTOR(11 DOWNTO 0);
+i2c_mem_douta : in STD_LOGIC_VECTOR(7 DOWNTO 0);
+o_kstoscale : OUT  std_logic_vector (31 downto 0);
+o_rdy : out std_logic
 );
 END COMPONENT;
-
-
---Inputs
-signal i_clock : std_logic := '0';
-signal i_reset : std_logic := '0';
-signal i_ee0x243f : std_logic_vector(15 downto 0) := (others => '0');
-
---Outputs
-signal o_kstoscale : std_logic_vector(31 downto 0);
+signal ExtractKsToScaleParameter_clock : std_logic;
+signal ExtractKsToScaleParameter_reset : std_logic;
+signal ExtractKsToScaleParameter_run : std_logic;
+signal ExtractKsToScaleParameter_i2c_mem_ena : STD_LOGIC;
+signal ExtractKsToScaleParameter_i2c_mem_addra : STD_LOGIC_VECTOR(11 DOWNTO 0);
+signal ExtractKsToScaleParameter_i2c_mem_douta : STD_LOGIC_VECTOR(7 DOWNTO 0);
+signal ExtractKsToScaleParameter_kstoscale : std_logic_vector (31 downto 0);
+signal ExtractKsToScaleParameter_rdy : std_logic;
 
 -- Clock period definitions
 constant i_clock_period : time := 10 ns;
 
 BEGIN
 
+inst_tb_i2c_mem : tb_i2c_mem
+PORT MAP (
+clka => ExtractKsToScaleParameter_clock,
+ena => ExtractKsToScaleParameter_i2c_mem_ena,
+wea => "0",
+addra => ExtractKsToScaleParameter_i2c_mem_addra,
+dina => (others => '0'),
+douta => ExtractKsToScaleParameter_i2c_mem_douta
+);
+
 -- Instantiate the Unit Under Test (UUT)
-uut: ExtractKsToScaleParameter PORT MAP (
-i_clock => i_clock,
-i_reset => i_reset,
-i_ee0x243f => i_ee0x243f,
-o_kstoscale => o_kstoscale
+uut: ExtractKsToScaleParameter port map (
+i_clock => ExtractKsToScaleParameter_clock,
+i_reset => ExtractKsToScaleParameter_reset,
+i_run => ExtractKsToScaleParameter_run,
+i2c_mem_ena => ExtractKsToScaleParameter_i2c_mem_ena,
+i2c_mem_addra => ExtractKsToScaleParameter_i2c_mem_addra,
+i2c_mem_douta => ExtractKsToScaleParameter_i2c_mem_douta,
+o_kstoscale => ExtractKsToScaleParameter_kstoscale,
+o_rdy => ExtractKsToScaleParameter_rdy
 );
 
 -- Clock process definitions
 i_clock_process :process
 begin
-i_clock <= '0';
+ExtractKsToScaleParameter_clock <= '0';
 wait for i_clock_period/2;
-i_clock <= '1';
+ExtractKsToScaleParameter_clock <= '1';
 wait for i_clock_period/2;
 end process;
 
 
 -- Stimulus process
 stim_proc: process
-begin		
+begin
 -- hold reset state for 100 ns.
-i_reset <= '1';
-wait for 100 ns;	
-i_reset <= '0';
+ExtractKsToScaleParameter_reset <= '1';
+wait for 100 ns;
+ExtractKsToScaleParameter_reset <= '0';
 wait for i_clock_period*10;
--- insert stimulus here 
-i_ee0x243f <= x"2889";
-wait for i_clock_period*10;
+-- insert stimulus here
+ExtractKsToScaleParameter_run <= '1'; wait for i_clock_period; ExtractKsToScaleParameter_run <= '0';
+wait until ExtractKsToScaleParameter_rdy = '1';
+wait for 1 ps;
 report "done" severity failure;
 end process;
 
