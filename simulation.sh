b9e08b775087629a6f787ac6b310f4f5815103e4
kedziorno <kjoblak85@gmail.com>
2023-10-15 17:28:49 +0200

    fix sim script

--- simulation.sh
+++ simulation.sh
@@ -3,5 +3,5 @@
 PROJECT="tb_test1"
 
 fuse -intstyle ise -incremental -lib secureip -o ./${PROJECT}_isim_beh.exe -prj ./${PROJECT}_beh.prj work.${PROJECT}
-./${PROJECT}_isim_beh.exe -log isim_output.txt
+./${PROJECT}_isim_beh.exe -log isim_output.txt -tclbatch isim.cmd
 
