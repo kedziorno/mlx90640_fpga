b8afeb2e7c0e2548f52e1c2a949de5382ce2c5bb
kedziorno <kjoblak85@gmail.com>
2024-09-21 22:03:15 +0200

    rewrite CalculateOffsetCP module

--- CalculateOffsetCP.vhd
+++ CalculateOffsetCP.vhd
@@ -20,7 +20,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-use work.p_fphdl_package1.all;
 use work.p_fphdl_package3.all;
 
 -- Uncomment the following library declaration if using
@@ -37,9 +36,9 @@ port (
 i_clock : in std_logic;
 i_reset : in std_logic;
 i_run : in std_logic;
-i_ee0x243a : in slv16; -- offcpsubpage1delta/offcpsubpage0 - 6/10bit
-o_offcpsubpage0 : out fd2ft;
-o_offcpsubpage1 : out fd2ft;
+i_ee0x243a : in std_logic_vector (15 downto 0); -- offcpsubpage1delta/offcpsubpage0 - 6/10bit
+o_offcpsubpage0 : out std_logic_vector (31 downto 0);
+o_offcpsubpage1 : out std_logic_vector (31 downto 0);
 o_rdy : out std_logic
 );
 end CalculateOffsetCP;
@@ -100,7 +99,7 @@ x"bf800000" when "111111",x"c0000000" when "111110",x"c0400000" when "111101",x"
 x"00000000" when others;
 
 p0 : process (i_clock,i_reset) is
-	variable fptmp1,fptmp2 : fd2ft;
+	variable fptmp1,fptmp2 : std_logic_vector (31 downto 0);
 	type states is (idle,
 	s1,s2,s3,s4,s5,s6,s7);
 	variable state : states;
