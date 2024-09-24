9f45d26fb23327f2291908fecdff91d68d4b358c
kedziorno <kjoblak85@gmail.com>
2024-09-24 16:56:52 +0200

    rewrite ExtractOffsetParameters module,use occrows loop

--- tb_ExtractOffsetParameters.vhd
+++ tb_ExtractOffsetParameters.vhd
@@ -306,7 +306,8 @@ wait until ExtractOffsetParameters_rdy = '1';
 --report "rdy at 716.155ns";
 --report "rdy at 715.905ns";
 --report "rdy at 692.715ns";
-report "rdy at 692.465ns";
+--report "rdy at 692.465ns";
+report "rdy at 692.595ns"; -- XXX occrows loop
 for i in 0 to 9 loop
 ExtractOffsetParameters_addr <= std_logic_vector (to_unsigned (datao.first(i).b, 10));
 wait until rising_edge (ExtractOffsetParameters_clock);
@@ -332,7 +333,8 @@ wait for 1 ps; -- must be for write
 --report "end at 716.815ns";
 --report "end at 716.565ns";
 --report "end at 693.375ns";
-report "end at 693.125ns";
+--report "end at 693.125ns";
+report "end at 693.255ns"; -- XXX occrows loop
 report "done" severity failure;
 --wait on o_done;
 end process;
