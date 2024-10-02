c462b51d46a1309899ab38f9140a510f8590d22d
kedziorno <kjoblak85@gmail.com>
2024-10-02 13:38:28 +0200

    rewrite CalculateAlphaCP module
    rewrite CalculateAlphaCP module
    
    rewrite CalculateAlphaCP module

--- CalculateAlphaCP.vhd
+++ CalculateAlphaCP.vhd
@@ -157,11 +157,10 @@ x"53000000" when x"c", x"53800000" when x"d", x"54000000" when x"e", x"54800000"
 x"00000000" when others;
 
 p0 : process (i_clock,i_reset) is
-	variable fptmp1,fptmp2 : std_logic_vector (31 downto 0);
 	type states is (idle,
-	s2,s3,s4,s5,s6,s9,s11);
+	s2,s3,s4,s5,s9,s11);
 	variable state : states;
-	variable ee0x2439 : std_logic_vector (15 downto 0);
+  variable acpsp0 : std_logic_vector (1 downto 0); -- Acpsubpage0 10bit
 begin
 	if (rising_edge (i_clock)) then
 		if (i_reset = '1') then
@@ -199,44 +198,40 @@ begin
 				when s2 => state := s3;
 					i2c_mem_addra <= std_logic_vector (to_unsigned (57*2+0, 12)); -- 2439 MSB Acpsubpage0 10bit/CP_P12P0_ratio 6bit
 				when s3 => state := s4;
-					nibble2 <= i2c_mem_douta (7 downto 4); -- Ascalecp 4bit
+					nibble2 <= i2c_mem_douta (7 downto 4); -- 2420 MSB Ascalecp 4bit
 					i2c_mem_addra <= std_logic_vector (to_unsigned (57*2+1, 12)); -- 2439 LSB Acpsubpage0 10bit/CP_P12P0_ratio 6bit
 				when s4 => state := s5;
-					ee0x2439 (15 downto 8) := i2c_mem_douta;
-				when s5 => state := s6;
-					ee0x2439 (7 downto 0) := i2c_mem_douta;
-				when s6 => state := s9;
-					nibble1 <= ee0x2439 (15 downto 10); -- CP_P12P0_ratio 6bit
-					mem_signed1024_ivalue <= ee0x2439 (9 downto 0); -- Acpsubpage0 10bit
+          nibble1 <= i2c_mem_douta (7 downto 2); -- CP_P12P0_ratio 6bit
+          acpsp0 := i2c_mem_douta (1 downto 0); -- Acpsubpage0 10-8bit
+				when s5 => state := s9;
+					mem_signed1024_ivalue <= acpsp0 & i2c_mem_douta (7 downto 0); -- Acpsubpage0 10-8 & 7-0
 				when s9 =>
 					divfpce_internal <= '1';
 					divfpa_internal <= mem_signed1024_ovalue; -- Acpsubpage0
 					divfpb_internal <= out_nibble2; -- 2^(Ascalecp+27)
 					divfpond_internal <= '1';
 					if (divfprdy_internal = '1') then state := s11;
-						fptmp1 := divfpr_internal;
 						divfpce_internal <= '0';
 						divfpond_internal <= '0';
 						divfpsclr_internal <= '1';
-						o_acpsubpage0 <= fptmp1;
+						o_acpsubpage0 <= divfpr_internal;
             --synthesis translate_off
-						report_error("================ calculateAlphaCP o_acpsubpage0", fptmp1, 0.0);
+						report_error("================ calculateAlphaCP o_acpsubpage0", divfpr_internal, 0.0);
             --synthesis translate_on
 					else state := s9; end if;
 				when s11 =>
 					divfpsclr_internal <= '0'; -- xxx to s9
 					mulfpce_internal <= '1';
-					mulfpa_internal <= fptmp1; -- Acpsubpage0/(2^(Ascalecp+27))
+					mulfpa_internal <= divfpr_internal; -- Acpsubpage0/(2^(Ascalecp+27))
 					mulfpb_internal <= out_nibble1; -- (1 + (CP_P12P0_ratio/2^7))
 					mulfpond_internal <= '1';
           if (mulfprdy_internal = '1') then state := idle;
-						fptmp1 := mulfpr_internal;
 						mulfpce_internal <= '0';
 						mulfpond_internal <= '0';
 						mulfpsclr_internal <= '1';
-						o_acpsubpage1 <= fptmp1;
+						o_acpsubpage1 <= mulfpr_internal;
             --synthesis translate_off
-						report_error("================ calculateAlphaCP o_acpsubpage1", fptmp1, 0.0);
+						report_error("================ calculateAlphaCP o_acpsubpage1", mulfpr_internal, 0.0);
             --synthesis translate_on
             o_rdy <= '1';
 					else state := s11; end if;
