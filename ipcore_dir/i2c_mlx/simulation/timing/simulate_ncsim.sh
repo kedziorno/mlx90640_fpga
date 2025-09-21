b37dba74db9f0e8d8b89ba08e518677f5b7fd030
kedziorno <kjoblak85@gmail.com>
2025-09-21 18:20:41 +0200

    eeprom coe i2c_mlx

--- ipcore_dir/i2c_mlx/simulation/timing/simulate_ncsim.sh
+++ ipcore_dir/i2c_mlx/simulation/timing/simulate_ncsim.sh
@@ -46,6 +46,7 @@
 # PART OF THIS FILE AT ALL TIMES.
 set work work
 #--------------------------------------------------------------------------------
+cp ../../../i2c_mlx.mif .
 mkdir work
 
 
--- ipcore_dir/i2c_mlx_xmdf.tcl
+++ ipcore_dir/i2c_mlx_xmdf.tcl
@@ -215,12 +215,12 @@ utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path i2c_mlx.asy
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type asy
 incr fcount
 
-utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path i2c_mlx.ngc
-utilities_xmdf::xmdfSetData $instance FileSet $fcount type ngc
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path i2c_mlx.mif
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type AnyView
 incr fcount
 
-utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path i2c_mlx.sym
-utilities_xmdf::xmdfSetData $instance FileSet $fcount type symbol
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path i2c_mlx.ngc
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type ngc
 incr fcount
 
 utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path i2c_mlx.vhd
