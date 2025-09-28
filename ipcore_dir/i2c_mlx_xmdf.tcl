bd14bfe1057eeb85484fa412c3cbad1389d259bd
kedziorno <kjoblak85@gmail.com>
2025-09-28 17:07:03 +0200

    wip - check signals with chipscope

--- ipcore_dir/i2c_mlx_xmdf.tcl
+++ ipcore_dir/i2c_mlx_xmdf.tcl
@@ -215,10 +215,6 @@ utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path i2c_mlx.asy
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type asy
 incr fcount
 
-utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path i2c_mlx.mif
-utilities_xmdf::xmdfSetData $instance FileSet $fcount type AnyView
-incr fcount
-
 utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path i2c_mlx.ngc
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type ngc
 incr fcount
--- tb/tb_test1.vhd
+++ tb/tb_test1.vhd
@@ -53,8 +53,8 @@ constant c_cold_start : integer := 1000;
 constant c_wait2 : integer := 1000;
 constant c_wait3 : integer := 10000;
 constant c_device : string (1 to 8) := "mlx90640"; -- mlx90640 (32x24),mlx90641 (16x12)
-constant c_calculate_type1 : string (1 to 13) := "c_temperature"; -- c_temperature,c_raws_images
-constant c_use_fisqrt1 : string (1 to 3) := " no" -- yes/no - depend from c_calculate_type(c_temperature)
+constant c_calculate_type1 : string (1 to 13) := "c_raws_images"; -- c_temperature,c_raws_images
+constant c_use_fisqrt1 : string (1 to 3) := "xxx" -- yes/no - depend from c_calculate_type(c_temperature)
 );
 PORT(
 i_clock : IN  std_logic;
