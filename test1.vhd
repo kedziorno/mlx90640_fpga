a1567dbacf56155c1f1eac4668cbf2fccbc9e430
kedziorno <kjoblak85@gmail.com>
2025-11-09 12:19:50 +0100

    fix tb, latch_data catch

--- test1.vhd
+++ test1.vhd
@@ -1683,27 +1683,29 @@ doutb => i2c_mlx_doutb
 --if (i_reset = '1') then
 --latch_data <= (others => '0');
 --elsif (falling_edge (melexis_mlx90640_i2c_mode2_ready)) then
-process (melexis_mlx90640_i2c_mode2_ready, i_reset) is
+process (clock_i, i_reset) is
 begin
 if (i_reset = '1') then
 latch_data <= (others => '0');
-elsif (falling_edge (melexis_mlx90640_i2c_mode2_ready)) then
-latch_data (0) <= melexis_mlx90640_i2c_bytes_to_recv(15);
-latch_data (1) <= melexis_mlx90640_i2c_bytes_to_recv(14);
-latch_data (2) <= melexis_mlx90640_i2c_bytes_to_recv(13);
-latch_data (3) <= melexis_mlx90640_i2c_bytes_to_recv(12);
-latch_data (4) <= melexis_mlx90640_i2c_bytes_to_recv(11);
-latch_data (5) <= melexis_mlx90640_i2c_bytes_to_recv(10);
-latch_data (6) <= melexis_mlx90640_i2c_bytes_to_recv(9);
-latch_data (7) <= melexis_mlx90640_i2c_bytes_to_recv(8);
-latch_data (8) <= melexis_mlx90640_i2c_bytes_to_recv(7);
-latch_data (9) <= melexis_mlx90640_i2c_bytes_to_recv(6);
-latch_data (10) <= melexis_mlx90640_i2c_bytes_to_recv(5);
-latch_data (11) <= melexis_mlx90640_i2c_bytes_to_recv(4);
-latch_data (12) <= melexis_mlx90640_i2c_bytes_to_recv(3);
-latch_data (13) <= melexis_mlx90640_i2c_bytes_to_recv(2);
-latch_data (14) <= melexis_mlx90640_i2c_bytes_to_recv(1);
-latch_data (15) <= melexis_mlx90640_i2c_bytes_to_recv(0);
+elsif (rising_edge (clock_i)) then
+if (melexis_mlx90640_i2c_mode2_ready = '1') then
+latch_data (15) <= melexis_mlx90640_i2c_bytes_to_recv(15);
+latch_data (14) <= melexis_mlx90640_i2c_bytes_to_recv(14);
+latch_data (13) <= melexis_mlx90640_i2c_bytes_to_recv(13);
+latch_data (12) <= melexis_mlx90640_i2c_bytes_to_recv(12);
+latch_data (11) <= melexis_mlx90640_i2c_bytes_to_recv(11);
+latch_data (10) <= melexis_mlx90640_i2c_bytes_to_recv(10);
+latch_data (9) <= melexis_mlx90640_i2c_bytes_to_recv(9);
+latch_data (8) <= melexis_mlx90640_i2c_bytes_to_recv(8);
+latch_data (7) <= melexis_mlx90640_i2c_bytes_to_recv(7);
+latch_data (6) <= melexis_mlx90640_i2c_bytes_to_recv(6);
+latch_data (5) <= melexis_mlx90640_i2c_bytes_to_recv(5);
+latch_data (4) <= melexis_mlx90640_i2c_bytes_to_recv(4);
+latch_data (3) <= melexis_mlx90640_i2c_bytes_to_recv(3);
+latch_data (2) <= melexis_mlx90640_i2c_bytes_to_recv(2);
+latch_data (1) <= melexis_mlx90640_i2c_bytes_to_recv(1);
+latch_data (0) <= melexis_mlx90640_i2c_bytes_to_recv(0);
+end if;
 end if;
 end process;
 
